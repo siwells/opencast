1261b26c7461146d4026edff2f2c93ea8c03886c
Simon Wells <siwells@gmail.com>
2012-04-30 17:47:55 +0100

    Added basic retrieval of redmine issues as JSON

--- src/opencast.py
+++ src/opencast.py
@@ -1,5 +1,22 @@
+import urllib2
 
+def retrieve_issues():
+    file_name = "design.json"
+    auth_handler = urllib2.HTTPBasicAuthHandler()
+    auth_handler.add_password(realm='Redmine API', uri='https://developers.superhub-project.eu/projects/wp1/issues.json', user='swells', passwd='marma1ad3')
 
+    opener = urllib2.build_opener(auth_handler)
+    urllib2.install_opener(opener)
+    try: 
+        result = urllib2.urlopen("https://developers.superhub-project.eu/projects/wp1/issues.json?tracker_id=14")#&limit=1")
+        with open(file_name, 'w') as file:
+            for line in result:
+                file.write(line)            
+
+    except urllib2.HTTPError, e:
+        print e.headers
+        #print e.headers['www-authenticate']
+        #print e
 
 
 if __name__ == '__main__':
